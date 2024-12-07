a8b953d17cbd89e9e861d6e3174209a43552effb
fred <fred.yah@mars-deg.com>
2024-12-07 07:38:17 +0000

    add buildings

---
+++ src/city.js
@@ -0,0 +1,28 @@
+export function createCity(size) {
+    const data = [];
+    initialize();
+
+    function initialize() {
+        for (let x = 0; x < size; x++) {
+            const column = [];
+            for (let y = 0; y < size; y++) {
+                const tile = {
+                    x,
+                    y,
+                    building: undefined
+                };
+                if (Math.random() > 0.7) {
+                    tile.building = 'building';
+                }
+                
+                column.push(tile);
+            }
+            data.push(column);
+        }
+    }
+
+    return {
+        size,
+        data
+    }
+}
